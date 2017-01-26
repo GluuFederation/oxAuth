c241323b3fd031c524abc6f81d988873bc20bf5e
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2017-01-26 11:38:33 +0200

    Re throw WebApplicationException in register client

--- Server/src/main/java/org/xdi/oxauth/register/ws/rs/RegisterRestWebServiceImpl.java
+++ Server/src/main/java/org/xdi/oxauth/register/ws/rs/RegisterRestWebServiceImpl.java
@@ -42,6 +42,7 @@ import org.xdi.util.security.StringEncrypter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
@@ -214,6 +215,9 @@ public class RegisterRestWebServiceImpl implements RegisterRestWebService {
         } catch (JSONException e) {
             builder = internalErrorResponse();
             log.error(e.getMessage(), e);
+        } catch (WebApplicationException e) {
+            log.error(e.getMessage(), e);
+            throw e;
         } catch (Exception e) {
             builder = internalErrorResponse();
             log.error(e.getMessage(), e);
