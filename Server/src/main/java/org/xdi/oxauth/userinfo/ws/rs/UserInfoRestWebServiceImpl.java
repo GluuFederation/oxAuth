5230eaeb677d3f88e407671242ff8f677c561b33
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2017-05-12 16:18:03 -0400

    oxAuth #518 NPE in UserInfoRestWebServiceImpl

--- Server/src/main/java/org/xdi/oxauth/userinfo/ws/rs/UserInfoRestWebServiceImpl.java
+++ Server/src/main/java/org/xdi/oxauth/userinfo/ws/rs/UserInfoRestWebServiceImpl.java
@@ -66,7 +66,7 @@ import java.util.*;
  * Provides interface for User Info REST web services
  *
  * @author Javier Rojas Blum
- * @version August 17, 2016
+ * @version May 12, 2017
  */
 @Name("requestUserInfoRestWebService")
 public class UserInfoRestWebServiceImpl implements UserInfoRestWebService {
@@ -134,6 +134,9 @@ public class UserInfoRestWebServiceImpl implements UserInfoRestWebService {
                 if (authorizationGrant == null) {
                     builder = Response.status(400);
                     builder.entity(errorResponseFactory.getErrorAsJson(UserInfoErrorResponseType.INVALID_TOKEN));
+                } else if (authorizationGrant.getAuthorizationGrantType() == AuthorizationGrantType.CLIENT_CREDENTIALS) {
+                    builder = Response.status(403);
+                    builder.entity(errorResponseFactory.getErrorAsJson(UserInfoErrorResponseType.INSUFFICIENT_SCOPE));
                 } else if (!authorizationGrant.getScopes().contains(DefaultScope.OPEN_ID.toString())
                         && !authorizationGrant.getScopes().contains(DefaultScope.PROFILE.toString())) {
                     builder = Response.status(403);
