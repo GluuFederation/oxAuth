c445cd822cc4a732747cdbe13a7723ee42ae2fe9
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-01-16 16:51:11 +0300

    Sync scripts with master

--- Server/integrations/passport/PassportExternalAuthenticator.py
+++ Server/integrations/passport/PassportExternalAuthenticator.py
@@ -157,9 +157,9 @@ class PersonAuthentication(PersonAuthenticationType):
                             newUser.setAttribute(localAttribute, localAttributeValue)
 
                     if "shibboleth" in self.getUserValueFromAuth("provider", requestParameters):
-                        newUser.setAttribute("oxExternalUid", "passport" + ":" + self.getUserValueFromAuth(self.getUidRemoteAttr(), requestParameters))
+                        newUser.setAttribute("oxExternalUid", "passport-saml" + ":" + self.getUserValueFromAuth(self.getUidRemoteAttr(), requestParameters))
                     else:
-                        newUser.setAttribute("oxExternalUid", self.getUserValueFromAuth("provider",requestParameters) + ":" + self.getUserValueFromAuth(self.getUidRemoteAttr(), requestParameters))
+                        newUser.setAttribute("oxExternalUid", "passport-"+ self.getUserValueFromAuth("provider",requestParameters) + ":" + self.getUserValueFromAuth(self.getUidRemoteAttr(), requestParameters))
 
                     print ("Passport: " + self.getUserValueFromAuth("provider",
                                                      requestParameters) + ": Attempting to add user " + self.getUserValueFromAuth(
@@ -178,9 +178,28 @@ class PersonAuthentication(PersonAuthenticationType):
 
                 else:
                     foundUserName = foundUser.getUserId()
-                    print("Passport: User Found " + str(foundUserName))
+                    print("Passport-social: User Found " + str(foundUserName))
+                    userService = CdiUtil.bean(UserService)
+
+                    for attributesMappingEntry in self.attributesMapping.entrySet():
+                        remoteAttribute = attributesMappingEntry.getKey()
+                        localAttribute = attributesMappingEntry.getValue()
+                        localAttributeValue = self.getUserValueFromAuth(remoteAttribute, requestParameters)
+                        if ((localAttribute != None) & (localAttributeValue != "undefined") & (
+                                    localAttribute != "provider")):
+                            try:
+                                value = foundUser.getAttributeValues(str(localAttribute))[0]
+
+                                if value != localAttributeValue:
+                                    userService.setCustomAttribute(foundUser,localAttribute,localAttributeValue)
+                                    userService.updateUser(foundUser)
+
+
+                            except Exception, err:
+                                print("Error in update Attribute " + str(err))
+
                     userAuthenticated = authenticationService.authenticate(foundUserName)
-                    print("Passport: Is user authenticated = " + str(userAuthenticated))
+                    print("Passport-social: Is user authenticated = " + str(userAuthenticated))
                     return True
 
             except Exception, err:
