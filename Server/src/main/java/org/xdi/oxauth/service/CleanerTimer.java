d6d111619f81f8404ee90878a1fb80ad22f3356d
yuriyz <yzabrovarniy>
2018-08-10 00:45:05 +0200

    oxauth : extracted cache cleanup into separate method
    https://github.com/GluuFederation/oxAuth/issues/654
    
    (cherry picked from commit 3890048)

--- Server/src/main/java/org/xdi/oxauth/service/CleanerTimer.java
+++ Server/src/main/java/org/xdi/oxauth/service/CleanerTimer.java
@@ -126,10 +126,12 @@ public class CleanerTimer {
         }
 
         try {
+            Date now = new Date();
+
+            processCache(now);
             processAuthorizationGrantList();
             processRegisteredClients();
 
-            Date now = new Date();
             this.umaRptService.cleanup(now);
             this.umaPermissionService.cleanup(now);
             this.umaPctService.cleanup(now);
@@ -139,13 +141,18 @@ public class CleanerTimer {
             processU2fDeviceRegistrations();
 
             processMetricEntries();
-
-            if (cacheService.isNativePersistenceCacheProvider()) {
-                ((NativePersistenceCacheProvider) cacheService.getCacheProvider()).cleanup(now, BATCH_SIZE);
-
-            }
         } finally {
             this.isActive.set(false);
+        }
+    }
+
+    private void processCache(Date now) {
+        try {
+            if (cacheService.isNativePersistenceCacheProvider()) {
+                ((NativePersistenceCacheProvider) cacheService.getCacheProvider()).cleanup(now, BATCH_SIZE);
+            }
+        } catch (Exception e) {
+            log.error("Failed to clean up cache.", e);
         }
     }
 
