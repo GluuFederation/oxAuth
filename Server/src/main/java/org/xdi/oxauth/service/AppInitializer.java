4a76a7dc6af82d87ccd357024aa921d2769148d0
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-12-20 18:19:57 +0300

    Fix script manager initialization

--- Server/src/main/java/org/xdi/oxauth/service/AppInitializer.java
+++ Server/src/main/java/org/xdi/oxauth/service/AppInitializer.java
@@ -191,7 +191,7 @@ public class AppInitializer {
 		// Initialize script manager
         List<CustomScriptType> supportedCustomScriptTypes = Arrays.asList(CustomScriptType.PERSON_AUTHENTICATION, CustomScriptType.CONSENT_GATHERING,
         		CustomScriptType.CLIENT_REGISTRATION, CustomScriptType.ID_GENERATOR, CustomScriptType.UMA_RPT_POLICY, CustomScriptType.UMA_CLAIMS_GATHERING,
-				CustomScriptType.APPLICATION_SESSION, CustomScriptType.DYNAMIC_SCOPE);
+				CustomScriptType.APPLICATION_SESSION, CustomScriptType.DYNAMIC_SCOPE, CustomScriptType.INTROSPECTION);
 
         // Start timer
         initSchedulerService();
