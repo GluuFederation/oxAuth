3bec6b349fdd9791198a0306ca0fea30635ca594
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2019-01-06 21:16:07 +0300

    Clean up authorization session is authenticatio nsession is not exists

--- Server/src/main/java/org/xdi/oxauth/session/ws/rs/EndSessionRestWebServiceImpl.java
+++ Server/src/main/java/org/xdi/oxauth/session/ws/rs/EndSessionRestWebServiceImpl.java
@@ -192,6 +192,9 @@ public class EndSessionRestWebServiceImpl implements EndSessionRestWebService {
             }
         }
 
+        // Clean up authorization session
+        removeConsentSessionId(httpRequest, httpResponse);
+
         SessionId ldapSessionId = removeSessionId(sessionId, httpRequest, httpResponse);
         if (ldapSessionId == null) {
             final String reason = "Failed to identify session by session_id query parameter or by session_id cookie.";
@@ -199,9 +202,6 @@ public class EndSessionRestWebServiceImpl implements EndSessionRestWebService {
             throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity(
                     errorResponseFactory.errorAsJson(EndSessionErrorResponseType.INVALID_REQUEST, reason)).build());
         }
-
-        // Clean up authorization session
-        removeConsentSessionId(httpRequest, httpResponse);
 
         boolean isExternalLogoutPresent;
         boolean externalLogoutResult = false;
