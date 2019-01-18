0c496e991a66aab283c30b393953b002c30b4e6a
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2019-01-18 22:23:20 +0300

    Update session AuthZ parameters on ACR change #984

--- Server/src/main/java/org/xdi/oxauth/service/SessionIdService.java
+++ Server/src/main/java/org/xdi/oxauth/service/SessionIdService.java
@@ -156,6 +156,7 @@ public class SessionIdService {
                     }
 
                     if (sessionAcrLevel < currentAcrLevel) {
+                        reinitLogin(session, false);
                         throw new AcrChangedException();
                     }
                 }
