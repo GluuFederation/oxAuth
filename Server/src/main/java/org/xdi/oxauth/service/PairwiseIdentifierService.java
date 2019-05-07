dab2d1796c5580ce750fb1bb6ca03897b686ebe0
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2019-05-07 11:49:54 -0400

    oxAuth #1063 Add a config value to allow to share the same sub between two Clients with the same sector identifier

--- Server/src/main/java/org/xdi/oxauth/service/PairwiseIdentifierService.java
+++ Server/src/main/java/org/xdi/oxauth/service/PairwiseIdentifierService.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 /**
  * @author Javier Rojas Blum
- * @version June 30, 2018
+ * @version May 7, 2019
  */
 @Stateless
 @Named
@@ -63,10 +63,16 @@ public class PairwiseIdentifierService {
             prepareBranch(userInum);
 
             String baseDnForPairwiseIdentifiers = getBaseDnForPairwiseIdentifiers(userInum);
-            Filter sectorIdentifierFilter = Filter.createEqualityFilter("oxSectorIdentifier", sectorIdentifier);
-            Filter clientIdFilter = Filter.createEqualityFilter("oxAuthClientId", clientId);
 
-            Filter filter = Filter.createANDFilter(sectorIdentifierFilter, clientIdFilter);
+            Filter filter = null;
+            if (appConfiguration.isShareSubjectIdBetweenClientsWithSameSectorId()) {
+                filter = Filter.createEqualityFilter("oxSectorIdentifier", sectorIdentifier);
+            } else {
+                Filter sectorIdentifierFilter = Filter.createEqualityFilter("oxSectorIdentifier", sectorIdentifier);
+                Filter clientIdFilter = Filter.createEqualityFilter("oxAuthClientId", clientId);
+
+                filter = Filter.createANDFilter(sectorIdentifierFilter, clientIdFilter);
+            }
 
             List<PairwiseIdentifier> entries = ldapEntryManager.findEntries(baseDnForPairwiseIdentifiers, PairwiseIdentifier.class, filter);
             if (entries != null && !entries.isEmpty()) {
@@ -82,7 +88,8 @@ public class PairwiseIdentifierService {
         } else { // PairwiseIdType.ALGORITHMIC
             String key = appConfiguration.getPairwiseCalculationKey();
             String salt = appConfiguration.getPairwiseCalculationSalt();
-            String localAccountId = userInum + clientId;
+            String localAccountId = appConfiguration.isShareSubjectIdBetweenClientsWithSameSectorId() ?
+                    userInum : userInum + clientId;
 
             String calculatedSub = SubjectIdentifierGenerator.generatePairwiseSubjectIdentifier(
                     sectorIdentifierUri, localAccountId, key, salt, appConfiguration);
