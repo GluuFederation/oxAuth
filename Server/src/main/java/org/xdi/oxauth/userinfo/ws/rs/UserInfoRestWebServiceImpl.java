369c1f6f8d5009aad0a296fb6a61eaba3e1c81d5
yurem <Yuriy.Movchan@gmail.com>
2019-10-29 16:22:46 +0300

    Add more debug to user info endpoint

--- Server/src/main/java/org/xdi/oxauth/userinfo/ws/rs/UserInfoRestWebServiceImpl.java
+++ Server/src/main/java/org/xdi/oxauth/userinfo/ws/rs/UserInfoRestWebServiceImpl.java
@@ -220,6 +220,8 @@ public class UserInfoRestWebServiceImpl implements UserInfoRestWebService {
 
     public String getJwtResponse(SignatureAlgorithm signatureAlgorithm, User user, AuthorizationGrant authorizationGrant,
                                  Collection<String> scopes) throws Exception {
+        log.trace("Building JWT reponse with next scopes {0} for user {1} and user custom attributes {0}", scopes, user.getUserId(), user.getCustomAttributes());
+
         Jwt jwt = new Jwt();
         AbstractCryptoProvider cryptoProvider = CryptoProviderFactory.getCryptoProvider(appConfiguration);
 
@@ -359,6 +361,8 @@ public class UserInfoRestWebServiceImpl implements UserInfoRestWebService {
     public String getJweResponse(
             KeyEncryptionAlgorithm keyEncryptionAlgorithm, BlockEncryptionAlgorithm blockEncryptionAlgorithm,
             User user, AuthorizationGrant authorizationGrant, Collection<String> scopes) throws Exception {
+        log.trace("Building JWE reponse with next scopes {0} for user {1} and user custom attributes {0}", scopes, user.getUserId(), user.getCustomAttributes());
+
         Jwe jwe = new Jwe();
 
         // Header
@@ -516,6 +520,8 @@ public class UserInfoRestWebServiceImpl implements UserInfoRestWebService {
      */
     public String getJSonResponse(User user, AuthorizationGrant authorizationGrant, Collection<String> scopes)
             throws Exception {
+        log.trace("Building JSON reponse with next scopes {0} for user {1} and user custom attributes {0}", scopes, user.getUserId(), user.getCustomAttributes());
+
         JsonWebResponse jsonWebResponse = new JsonWebResponse();
 
         // Claims
