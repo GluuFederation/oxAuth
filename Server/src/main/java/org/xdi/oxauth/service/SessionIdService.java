GluuFederation/oxAuth

commit 212183564c4ad4d7859b7b2cc6e510577ecf2e8e

Merge session fix

Server/src/main/java/org/xdi/oxauth/service/SessionIdService.java
@@ -62,8 +62,24 @@ import org.xdi.oxauth.util.ServerUtil;
 import org.xdi.service.CacheService;
 import org.xdi.util.StringHelper;
 
-import com.unboundid.ldap.sdk.LDAPException;
-import com.unboundid.ldap.sdk.ResultCode;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Yuriy Zabrovarnyy
@@ -516,61 +532,24 @@ public class SessionIdService {
     public SessionId generateUnauthenticatedSessionId(String userDn, Date authenticationDate, SessionIdState state, Map<String, String> sessionIdAttributes, boolean persist) {
         return generateSessionId(userDn, authenticationDate, state, sessionIdAttributes, persist);
     }
+    
+    public String computeSessionState(SessionId sessionId, String clientId, String redirectUri) {
+        final boolean isSameClient = clientId.equals(sessionId.getSessionAttributes().get("client_id")) &&
+                redirectUri.equals(sessionId.getSessionAttributes().get("redirect_uri"));
+        if(isSameClient)
+            return sessionId.getSessionState();
+        final String salt = UUID.randomUUID().toString();
+        final String opbs = sessionId.getOPBrowserState();
+        final String sessionState = computeSessionState(clientId,redirectUri, opbs, salt);
+        return sessionState;
+    }
 
-    private SessionId generateSessionId(String userDn, Date authenticationDate, SessionIdState state, Map<String, String> sessionIdAttributes, boolean persist) {
+    private String computeSessionState(String clientId, String redirectUri, String opbs, String salt) {
         try {
-            final String sid = UUID.randomUUID().toString();
-            final String salt = UUID.randomUUID().toString();
-            final String clientId = sessionIdAttributes.get("client_id");
-            final String opbs = UUID.randomUUID().toString();
+            final String clientOrigin = getClientOrigin(redirectUri);
             final String sessionState = JwtUtil.bytesToHex(JwtUtil.getMessageDigestSHA256(
-                    clientId + " " + appConfiguration.getIssuer() + " " + opbs + " " + salt)) + "." + salt;
-            final String dn = dn(sid);
-            sessionIdAttributes.put(OP_BROWSER_STATE, opbs);
-
-            if (StringUtils.isBlank(dn)) {
-                return null;
-            }
-
-            if (SessionIdState.AUTHENTICATED == state) {
-                if (StringUtils.isBlank(userDn)) {
-                    return null;
-                }
-            }
-
-            final SessionId sessionId = new SessionId();
-            sessionId.setId(sid);
-            sessionId.setDn(dn);
-            sessionId.setUserDn(userDn);
-            sessionId.setSessionState(sessionState);
-
-            Boolean sessionAsJwt = appConfiguration.getSessionAsJwt();
-            sessionId.setIsJwt(sessionAsJwt != null && sessionAsJwt);
-
-            if (authenticationDate != null) {
-                sessionId.setAuthenticationTime(authenticationDate);
-            }
-
-            if (state != null) {
-                sessionId.setState(state);
-            }
-
-            sessionId.setSessionAttributes(sessionIdAttributes);
-            sessionId.setLastUsedAt(new Date());
-
-            if (sessionId.getIsJwt()) {
-                sessionId.setJwt(generateJwt(sessionId, userDn).asString());
-            }
-
-            boolean persisted = false;
-            if (persist) {
-                persisted = persistSessionId(sessionId);
-            }
-
-            auditLogging(sessionId);
-
-            log.trace("Generated new session, id = '{}', state = '{}', asJwt = '{}', persisted = '{}'", sessionId.getId(), sessionId.getState(), sessionId.getIsJwt(), persisted);
-            return sessionId;
+                    clientId + " " + clientOrigin + " " + opbs + " " + salt)) + "." + salt;
+            return sessionState;
         } catch (NoSuchProviderException e) {
             log.error("Failed generating session state! " + e.getMessage(), e);
             throw new RuntimeException(e);
@@ -580,7 +559,73 @@ public class SessionIdService {
         } catch (UnsupportedEncodingException e) {
             log.error("Failed generating session state! " + e.getMessage(), e);
             throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            log.error("Failed generating session state! " + e.getMessage(), e);
+            throw new RuntimeException(e);
         }
+    }
+
+    private String getClientOrigin(String redirectUri) throws URISyntaxException {
+        final URI uri = new URI(redirectUri);
+        String result = uri.getScheme() + "://" + uri.getHost();
+        if(uri.getPort() > 0)
+            result += ":" + Integer.toString(uri.getPort());
+        return result;
+    }
+
+    private SessionId generateSessionId(String userDn, Date authenticationDate, SessionIdState state, Map<String, String> sessionIdAttributes, boolean persist) {
+        final String sid = UUID.randomUUID().toString();
+        final String salt = UUID.randomUUID().toString();
+        final String clientId = sessionIdAttributes.get("client_id");
+        final String opbs = UUID.randomUUID().toString();
+        final String redirectUri = sessionIdAttributes.get("redirect_uri");
+        final String sessionState = computeSessionState(clientId, redirectUri, opbs, salt);
+        final String dn = dn(sid);
+        sessionIdAttributes.put(OP_BROWSER_STATE, opbs);
+
+        if (StringUtils.isBlank(dn)) {
+            return null;
+        }
+
+        if (SessionIdState.AUTHENTICATED == state) {
+            if (StringUtils.isBlank(userDn)) {
+                return null;
+            }
+        }
+
+        final SessionId sessionId = new SessionId();
+        sessionId.setId(sid);
+        sessionId.setDn(dn);
+        sessionId.setUserDn(userDn);
+        sessionId.setSessionState(sessionState);
+
+        Boolean sessionAsJwt = appConfiguration.getSessionAsJwt();
+        sessionId.setIsJwt(sessionAsJwt != null && sessionAsJwt);
+
+        if (authenticationDate != null) {
+            sessionId.setAuthenticationTime(authenticationDate);
+        }
+
+        if (state != null) {
+            sessionId.setState(state);
+        }
+
+        sessionId.setSessionAttributes(sessionIdAttributes);
+        sessionId.setLastUsedAt(new Date());
+
+        if (sessionId.getIsJwt()) {
+            sessionId.setJwt(generateJwt(sessionId, userDn).asString());
+        }
+
+        boolean persisted = false;
+        if (persist) {
+            persisted = persistSessionId(sessionId);
+        }
+
+        auditLogging(sessionId);
+
+        log.trace("Generated new session, id = '{}', state = '{}', asJwt = '{}', persisted = '{}'", sessionId.getId(), sessionId.getState(), sessionId.getIsJwt(), persisted);
+        return sessionId;
     }
 
     private Jwt generateJwt(SessionId sessionId, String audience) {

Server/src/main/java/org/xdi/oxauth/authorize/ws/rs/AuthorizeRestWebServiceImpl.java
@@ -636,7 +636,7 @@ public class AuthorizeRestWebServiceImpl implements AuthorizeRestWebService {
                                     log.trace("newSessionId = {}", newSessionId);
                                 }
                                 redirectUriResponse.addResponseParameter(AuthorizeResponseParam.SESSION_ID, sessionUser.getId());
-                                redirectUriResponse.addResponseParameter(AuthorizeResponseParam.SESSION_STATE, sessionUser.getSessionState());
+                                redirectUriResponse.addResponseParameter(AuthorizeResponseParam.SESSION_STATE, sessionIdService.computeSessionState(sessionUser, clientId, redirectUri));
                                 redirectUriResponse.addResponseParameter(AuthorizeResponseParam.STATE, state);
                                 if (scope != null && !scope.isEmpty()) {
                                     scope = authorizationGrant.checkScopesPolicy(scope);

Server/src/main/java/org/xdi/oxauth/session/ws/rs/EndSessionRestWebServiceImpl.java
@@ -118,8 +118,9 @@ public class EndSessionRestWebServiceImpl implements EndSessionRestWebService {
         log.debug(reason);
         try {
             if (allowPostLogoutRedirect(postLogoutRedirectUri)) {
-                String separator = postLogoutRedirectUri.contains("?") ? "&" : "?";
-                postLogoutRedirectUri = postLogoutRedirectUri + separator + errorResponseFactory.getErrorAsQueryString(error, "", reason);
+                // Commented out to avoid sending an error:                
+                // String separator = postLogoutRedirectUri.contains("?") ? "&" : "?";
+                // postLogoutRedirectUri = postLogoutRedirectUri + separator + errorResponseFactory.getErrorAsQueryString(error, "", reason);
                 return Response.status(Response.Status.FOUND).location(new URI(postLogoutRedirectUri)).build();
             }
         } catch (URISyntaxException e) {

commit 788635467739647e0f5a87d91b0935cd3ddce8df

fixed bad PR merge (caused by https://github.com/GluuFederation/oxAuth/commit/212183564c4ad4d7859b7b2cc6e510577ecf2e8e)

Server/src/main/java/org/xdi/oxauth/service/SessionIdService.java
@@ -6,30 +6,8 @@
 
 package org.xdi.oxauth.service;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
-import javax.enterprise.context.RequestScoped;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.ResultCode;
 import org.apache.commons.lang.StringUtils;
 import org.codehaus.jettison.json.JSONException;
 import org.gluu.site.ldap.persistence.exception.EmptyEntryPersistenceException;
@@ -566,16 +544,13 @@ public class SessionIdService {
     }
 
     private String getClientOrigin(String redirectUri) throws URISyntaxException {
-        try {
-            final String clientOrigin = getClientOrigin(redirectUri);
-            final String sessionState = JwtUtil.bytesToHex(JwtUtil.getMessageDigestSHA256(
-                    clientId + " " + clientOrigin + " " + opbs + " " + salt)) + "." + salt;
-            return sessionState;
-        } catch (NoSuchProviderException | NoSuchAlgorithmException | UnsupportedEncodingException | URISyntaxException e) {
-            log.error("Failed generating session state! " + e.getMessage(), e);
-            throw new RuntimeException(e);
-	}
+        final URI uri = new URI(redirectUri);
+        String result = uri.getScheme() + "://" + uri.getHost();
+        if(uri.getPort() > 0)
+            result += ":" + Integer.toString(uri.getPort());
+        return result;
     }
+
 
     private SessionId generateSessionId(String userDn, Date authenticationDate, SessionIdState state, Map<String, String> sessionIdAttributes, boolean persist) {
         final String sid = UUID.randomUUID().toString();