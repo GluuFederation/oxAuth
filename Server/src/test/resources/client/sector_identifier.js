e579966eef7e723e5547bbfa6bcb76a9cd301bd1
pujavs <puja@gluu.org>
2020-02-27 23:30:01 +0530

    swagger.yaml changes

--- Server/src/test/resources/client/sector_identifier.js
+++
@@ -1,7 +0,0 @@
-[ "https://${test.server.name}/oxauth-rp/home.htm",
-    "https://client.example.org/callback",
-    "https://client.example.org/callback2",
-    "https://client.other_company.example.net/callback",
-    "https://client.example.com/cb",
-    "https://client.example.com/cb1",
-    "https://client.example.com/cb2" ]
--- install.bat
+++
@@ -1,1 +0,0 @@
-mvn -Dmaven.test.skip=true -Ddependency-check.skip=true clean compile install
