7bd0076be65c0c857479ea656d7ca73c83d6aba0
YuriyZ <yzabrovarniy@gmail.com>
2021-07-26 09:50:59 +0300

    feat(4.3): added statAuthorizationScope configuration property and enforced it
    https://github.com/GluuFederation/oxAuth/issues/1554

--- Server/src/main/java/org/gluu/oxauth/ws/rs/stat/StatWS.java
+++ Server/src/main/java/org/gluu/oxauth/ws/rs/stat/StatWS.java
@@ -9,7 +9,6 @@ import org.gluu.oxauth.model.common.AbstractToken;
 import org.gluu.oxauth.model.common.AuthorizationGrant;
 import org.gluu.oxauth.model.configuration.AppConfiguration;
 import org.gluu.oxauth.model.error.ErrorResponseFactory;
-import org.gluu.oxauth.model.session.SessionClient;
 import org.gluu.oxauth.model.stat.StatEntry;
 import org.gluu.oxauth.model.token.TokenErrorResponseType;
 import org.gluu.oxauth.security.Identity;
@@ -225,9 +224,9 @@ public class StatWS {
             throw errorResponseFactory.createWebApplicationException(Response.Status.UNAUTHORIZED, TokenErrorResponseType.ACCESS_DENIED, "Token expired.");
         }
 
-        if (!grant.getScopesAsString().contains("stat")) {
-            log.trace("Access Token does NOT have 'stat' scope which is required to call Statistic Endpoint.");
-            throw errorResponseFactory.createWebApplicationException(Response.Status.UNAUTHORIZED, TokenErrorResponseType.ACCESS_DENIED, "stat scope is required for token.");
+        if (!grant.getScopesAsString().contains(appConfiguration.getStatAuthorizationScope())) {
+            log.trace("Access Token does NOT have '" + appConfiguration.getStatAuthorizationScope() + "' scope which is required to call Statistic Endpoint.");
+            throw errorResponseFactory.createWebApplicationException(Response.Status.UNAUTHORIZED, TokenErrorResponseType.ACCESS_DENIED, appConfiguration.getStatAuthorizationScope() + " scope is required for token.");
         }
     }
 
