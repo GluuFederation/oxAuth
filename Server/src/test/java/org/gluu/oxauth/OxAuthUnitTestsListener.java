3216962725e8153bd76077df8514c10c68978adc
SMan <smansoft@ukr.net>
2022-01-05 23:41:06 -0600

    fix: data_provider, redundant calls of u test functions, listener has been added #1595;

---
+++ Server/src/test/java/org/gluu/oxauth/OxAuthUnitTestsListener.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package org.gluu.oxauth;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+import com.google.common.base.Throwables;
+
+/**
+ * @author Sergey Manoylo
+ * @version January 5, 2022
+ */
+public class OxAuthUnitTestsListener implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+        Reporter.log("Test STARTED: " + result.getName() + "." + result.getMethod().getMethodName(), true);		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+        Reporter.log("Test SUCCESS: " + result.getName() + "." + result.getMethod().getMethodName(), true);
+        Reporter.log("", true);		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+        Reporter.log("Test FAILED: " + result.getName() + "." + result.getMethod().getMethodName(), true);
+        testFailed(result);
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+        Reporter.log("Test SKIPPED: " + result.getName() + "." + result.getMethod().getMethodName(), true);
+        Reporter.log("", true);		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		Reporter.log("Test FAILED with Success Percentage: " + result.getName() + "." + result.getMethod().getMethodName(), true);
+        testFailed(result);		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+	}
+	
+    private void testFailed(ITestResult result) {
+        Object[] parameters = result.getParameters();
+        if(parameters != null) {
+            Reporter.log("Test Parameters: ", true);
+            for(Object parameter : parameters) {
+                Reporter.log("parameter = " + parameter, true);
+            }
+        }
+        Throwable throwable = result.getThrowable();
+        if(throwable != null) {
+            Reporter.log("", true);
+            Reporter.log("Exception: ", true);
+            Reporter.log(Throwables.getStackTraceAsString(result.getThrowable()), true);
+            Reporter.log("", true);
+        }
+    }	
+}
