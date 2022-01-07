d87e28836ff20d2072df33e1bd89aafa08694dcb
SMan <smansoft@ukr.net>
2022-01-07 17:44:30 -0600

    fix:  get_test_info has been added #1595;

--- Server/src/test/java/org/gluu/oxauth/OxAuthUnitTestsListener.java
+++ Server/src/test/java/org/gluu/oxauth/OxAuthUnitTestsListener.java
@@ -16,43 +16,43 @@ import com.google.common.base.Throwables;
  */
 public class OxAuthUnitTestsListener implements ITestListener {
 
-	@Override
-	public void onTestStart(ITestResult result) {
-        Reporter.log("Test STARTED: " + result.getName() + "." + result.getMethod().getMethodName(), true);		
-	}
+    @Override
+    public void onTestStart(ITestResult result) {
+        Reporter.log("Test STARTED: " + getTestInfo(result), true);		
+    }
 
-	@Override
-	public void onTestSuccess(ITestResult result) {
-        Reporter.log("Test SUCCESS: " + result.getName() + "." + result.getMethod().getMethodName(), true);
-        Reporter.log("", true);		
-	}
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        Reporter.log("Test SUCCESS: " + getTestInfo(result), true);
+        Reporter.log("", true);	
+    }
 
-	@Override
-	public void onTestFailure(ITestResult result) {
-        Reporter.log("Test FAILED: " + result.getName() + "." + result.getMethod().getMethodName(), true);
+    @Override
+    public void onTestFailure(ITestResult result) {
+        Reporter.log("Test FAILED: " + getTestInfo(result), true);
         testFailed(result);
-	}
+    }
 
-	@Override
-	public void onTestSkipped(ITestResult result) {
-        Reporter.log("Test SKIPPED: " + result.getName() + "." + result.getMethod().getMethodName(), true);
-        Reporter.log("", true);		
-	}
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        Reporter.log("Test SKIPPED: " + getTestInfo(result), true);
+        Reporter.log("", true);	
+    }
 
-	@Override
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		Reporter.log("Test FAILED with Success Percentage: " + result.getName() + "." + result.getMethod().getMethodName(), true);
-        testFailed(result);		
-	}
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        Reporter.log("Test FAILED with Success Percentage: " + getTestInfo(result), true);
+        testFailed(result);	
+    }
 
-	@Override
-	public void onStart(ITestContext context) {
-	}
+    @Override
+    public void onStart(ITestContext context) {
+    }
 
-	@Override
-	public void onFinish(ITestContext context) {
-	}
-	
+    @Override
+    public void onFinish(ITestContext context) {
+    }
+
     private void testFailed(ITestResult result) {
         Object[] parameters = result.getParameters();
         if(parameters != null) {
@@ -68,5 +68,16 @@ public class OxAuthUnitTestsListener implements ITestListener {
             Reporter.log(Throwables.getStackTraceAsString(result.getThrowable()), true);
             Reporter.log("", true);
         }
-    }	
+    }
+
+    private String getTestInfo(ITestResult result) {
+        String testInfo = null;
+        if(result.getClass() != null && result.getMethod() != null) {
+            testInfo = result.getClass().getName() + "." + result.getMethod().getMethodName();
+        }
+        else {
+            testInfo = result.getName();
+        }
+        return testInfo;
+    }
 }
