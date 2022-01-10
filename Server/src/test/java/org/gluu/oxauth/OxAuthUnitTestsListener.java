124df46b29bfbb1662ca0c66c2629b2de5d51745
SMan <smansoft@ukr.net>
2022-01-09 19:15:20 -0600

    fix: copyrights have been added #1595;

--- Server/src/test/java/org/gluu/oxauth/OxAuthUnitTestsListener.java
+++ Server/src/test/java/org/gluu/oxauth/OxAuthUnitTestsListener.java
@@ -1,5 +1,7 @@
-/**
- * 
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2022, Gluu
  */
 package org.gluu.oxauth;
 
