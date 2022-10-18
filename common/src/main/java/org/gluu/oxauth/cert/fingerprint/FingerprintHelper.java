c9f790a701b9f22cc33317901e36bcba0fc02d23
SMan <smansoft@ukr.net>
2022-10-17 22:56:08 -0500

    refactor: formatting and refactoring the code;

--- common/src/main/java/org/gluu/oxauth/cert/fingerprint/FingerprintHelper.java
+++ common/src/main/java/org/gluu/oxauth/cert/fingerprint/FingerprintHelper.java
@@ -28,37 +28,44 @@ import org.slf4j.LoggerFactory;
  */
 public class FingerprintHelper {
 
-	@SuppressWarnings("unused")
+    @SuppressWarnings("unused")
     private static final Logger log = LoggerFactory.getLogger(FingerprintHelper.class);
 
-	/*
-	 * Return SSH RSA public key fingerprint
-	 */
-	public static String getPublicKeySshFingerprint(PublicKey publicKey) throws NoSuchAlgorithmException, IOException {
-		if (publicKey instanceof RSAPublicKey) {
-	        MessageDigest digest = MessageDigest.getInstance("MD5");
-	        byte[] derEncoded = getDerEncoding((RSAPublicKey)publicKey);
-	        byte[] fingerprint = digest.digest(derEncoded);
-	        return Hex.encodeHexString(fingerprint);
-		}
-		else if (publicKey instanceof ECPublicKey) {
+    /*
+     * Return SSH RSA public key fingerprint
+     */
+    public static String getPublicKeySshFingerprint(PublicKey publicKey) throws NoSuchAlgorithmException, IOException {
+    if (publicKey instanceof RSAPublicKey || publicKey instanceof ECPublicKey) {
             MessageDigest digest = MessageDigest.getInstance("MD5");
-            byte[] derEncoded = getDerEncoding((ECPublicKey)publicKey);
+            byte[] derEncoded = null;                
+            if(publicKey instanceof RSAPublicKey) {
+                derEncoded = getDerEncoding((RSAPublicKey)publicKey);
+            }
+            else if (publicKey instanceof ECPublicKey) {
+                derEncoded = getDerEncoding((ECPublicKey)publicKey);
+            }
             byte[] fingerprint = digest.digest(derEncoded);
             return Hex.encodeHexString(fingerprint);
         }
-		throw new NoSuchAlgorithmException("Unsopported PublicKey type");
-	}
+        throw new NoSuchAlgorithmException("Unsopported PublicKey type");
+    }
 
-	private static byte[] getDerEncoding(RSAPublicKey key) throws IOException {
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-		DataOutputStream dataOutput = new DataOutputStream(buffer);
-		writeDataWithLength("ssh-rsa".getBytes(), dataOutput);
-		writeDataWithLength(key.getPublicExponent().toByteArray(), dataOutput);
-		writeDataWithLength(key.getModulus().toByteArray(), dataOutput);
-		return buffer.toByteArray();
-	}
+    private static byte[] getDerEncoding(RSAPublicKey key) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        DataOutputStream dataOutput = new DataOutputStream(buffer);
+        writeDataWithLength("ssh-rsa".getBytes(), dataOutput);
+        writeDataWithLength(key.getPublicExponent().toByteArray(), dataOutput);
+        writeDataWithLength(key.getModulus().toByteArray(), dataOutput);
+        return buffer.toByteArray();
+    }
 
+    /* 
+     * here is a pseudo SSH encoding (start string is 'ssh-ecdsa'), as format of SSH for EC something like that:
+     *    ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCzhFSVzzMlfUih30HB8k7Dpmn95NWQMQRcYpHWhIcMsXQL7TFD8izlr0bRA9XTmBDyKhgA80XMr33r4jeiP+MI=
+     * or for EDDSA (ed25519) 
+     *    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPZ6PEne43qIHzRihLo4wbo9+E7IFgPp7HLYLFoDiXz
+     * but current encoding is enough for generating a unique fingerprint.
+     */
     private static byte[] getDerEncoding(ECPublicKey key) throws IOException {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         DataOutputStream dataOutput = new DataOutputStream(buffer);
@@ -67,9 +74,9 @@ public class FingerprintHelper {
         return buffer.toByteArray();
     }
 
-	private static void writeDataWithLength(byte[] data, DataOutput byteBuffer) throws IOException {
-		byteBuffer.writeInt(data.length);
-		byteBuffer.write(data);
-	}
+    private static void writeDataWithLength(byte[] data, DataOutput byteBuffer) throws IOException {
+        byteBuffer.writeInt(data.length);
+        byteBuffer.write(data);
+    }
 
 }
